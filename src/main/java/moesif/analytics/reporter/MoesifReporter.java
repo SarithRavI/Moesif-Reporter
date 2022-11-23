0d30ca13570926f3c85460793ea6a779cd74827c
SarithRavI <imaduwagesarith932@gmail.com>
2022-11-23 22:16:24 +0000

    Modified Moesif Event Builder.

--- src/main/java/moesif/analytics/reporter/MoesifReporter.java
+++ src/main/java/moesif/analytics/reporter/MoesifReporter.java
@@ -1,14 +1,14 @@
-package moesif.analytics;
+package moesif.analytics.reporter;
 
 import com.moesif.api.MoesifAPIClient;
 import com.moesif.api.controllers.APIController;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.wso2.am.analytics.publisher.exception.MetricCreationException;
-
-import org.wso2.am.analytics.publisher.reporter.*;
-
+import org.wso2.am.analytics.publisher.reporter.CounterMetric;
+import org.wso2.am.analytics.publisher.reporter.MetricReporter;
+import org.wso2.am.analytics.publisher.reporter.MetricSchema;
+import org.wso2.am.analytics.publisher.reporter.TimerMetric;
 
 import java.util.Map;
 
@@ -17,18 +17,21 @@ public class MoesifReporter implements MetricReporter {
     private final APIController api;
 
     private static final Logger log = LoggerFactory.getLogger(MoesifReporter.class);
-
+    private final Map<String,String> properties;
     public MoesifReporter(Map<String, String> properties) {
-        String ID = "eyJhcHAiOiI2NjA6MzU2IiwidmVyIjoiMi4wIiwib3JnIjoiMjkxOjMyMSIsImlhdCI6MTY1OTMxMjAwMH0.ftR87qJRhnc-GwXdTIJZv5zQ2s08BnUU5UxRLARksZg";
+        // String ID = "eyJhcHAiOiI0NDI6Mzk4IiwidmVyIjoiMi4wIiwib3JnIjoiMjkxOjM1MSIsImlhdCI6MTY2MTk5MDQwMH0.Mj2x1Y2unzlmzYb9qLRmAtvE2STtxV7qv9uZSdQLIl4";
+        String ID = "eyJhcHAiOiIxMDUxOjI1NSIsInZlciI6IjIuMCIsIm9yZyI6IjM1MToyNzciLCJpYXQiOjE2NjcyNjA4MDB9.rPbXw_lU6E5-5Ws-DG2uhiIEIecTMBtkQpJpSBATt5o";
+
         client = new MoesifAPIClient(ID);
 
         api = APIController.getInstance();
         log.info("Successfully initialized");
+        this.properties = properties;
     }
 
     @Override
     public CounterMetric createCounterMetric(String name, MetricSchema metricSchema) throws MetricCreationException {
-        MoesifLogCounter logCounterMetric = new MoesifLogCounter(name, metricSchema,api);
+        MoesifLogCounter logCounterMetric = new MoesifLogCounter(name, metricSchema,api,this.properties);
 
         return logCounterMetric;
     }
