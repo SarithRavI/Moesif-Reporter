103cb6e2b5c2e85bc0e236ed368d81ec4d7ce6d3
SarithRavI <imaduwagesarith932@gmail.com>
2023-01-13 19:07:03 +0000

    Making production.

--- src/main/java/moesif/analytics/reporter/MoesifKeyRetriever.java
+++ src/main/java/moesif/analytics/reporter/MoesifKeyRetriever.java
@@ -7,7 +7,6 @@ import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 import java.util.Collection;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -83,7 +82,7 @@ public class MoesifKeyRetriever {
     }
     public  void callListResource() throws IOException {
         URL obj = new URL(MoesifMicroserviceConstants.LIST_URL);
-        String auth = "admin"+ ":" + "admin";
+        String auth = "";
         String encodedAuth = Base64.getEncoder().encodeToString(auth.getBytes(StandardCharsets.UTF_8));
         String authHeaderValue = "Basic " + encodedAuth;
 
@@ -113,7 +112,7 @@ public class MoesifKeyRetriever {
         String url = MoesifMicroserviceConstants.DETAIL_URL + "?" + MoesifMicroserviceConstants.QUERY_PARAM + "=" +
                 orgID;
         URL obj = new URL(url);
-        String auth = "admin"+ ":" + "admin";
+        String auth = "";
         String encodedAuth = Base64.getEncoder().encodeToString(auth.getBytes(StandardCharsets.UTF_8));
         String authHeaderValue = "Basic " + encodedAuth;
 
