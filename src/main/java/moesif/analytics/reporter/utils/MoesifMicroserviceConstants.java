5a8bd6ff172b2a42b611130a95551f015e94ff63
SarithRavI <imaduwagesarith932@gmail.com>
2023-01-16 16:41:42 +0000

    Added constants.

--- src/main/java/moesif/analytics/reporter/utils/MoesifMicroserviceConstants.java
+++ src/main/java/moesif/analytics/reporter/utils/MoesifMicroserviceConstants.java
@@ -7,6 +7,9 @@ public class MoesifMicroserviceConstants {
     public final static String QUERY_PARAM = "org_id";
     public final static int NUM_RETRY_ATTEMPTS = 3;
     public final static long TIME_TO_WAIT = 10;
+    public final static int NUM_RETRY_ATTEMPTS_PUBLISH = 3;
+    public final static long TIME_TO_WAIT_PUBLISH = 10;
+
 
 
 }
